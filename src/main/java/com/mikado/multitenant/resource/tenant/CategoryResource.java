5cd0f2f7bb08807230ec17816b1008ce19341e90
kolege <a.kolege@gmail.com>
2020-12-17 01:08:43 +0300

    CategoryResource comments deleted

--- src/main/java/com/mikado/multitenant/resource/tenant/CategoryResource.java
+++ src/main/java/com/mikado/multitenant/resource/tenant/CategoryResource.java
@@ -43,13 +43,6 @@ public class CategoryResource {
         this.categoryService = categoryService;
     }
 
-    /**
-     * {@code POST  /categories} : Create a new category.
-     *
-     * @param categoryDTO the categoryDTO to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new categoryDTO, or with status {@code 400 (Bad Request)} if the category has already an ID.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
     @PostMapping("/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) throws URISyntaxException {
         log.debug("REST request to save Category : {}", categoryDTO);
@@ -62,15 +55,6 @@ public class CategoryResource {
             .body(result);
     }
 
-    /**
-     * {@code PUT  /categories} : Updates an existing category.
-     *
-     * @param categoryDTO the categoryDTO to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated categoryDTO,
-     * or with status {@code 400 (Bad Request)} if the categoryDTO is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the categoryDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
     @PutMapping("/categories")
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO) throws URISyntaxException {
         log.debug("REST request to update Category : {}", categoryDTO);
@@ -83,12 +67,6 @@ public class CategoryResource {
             .body(result);
     }
 
-    /**
-     * {@code GET  /categories} : get all the categories.
-     *
-     * @param pageable the pagination information.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of categories in body.
-     */
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryDTO>> getAllCategories(Pageable pageable) {
         log.debug("REST request to get a page of Categories");
@@ -97,12 +75,6 @@ public class CategoryResource {
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
 
-    /**
-     * {@code GET  /categories/:id} : get the "id" category.
-     *
-     * @param id the id of the categoryDTO to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the categoryDTO, or with status {@code 404 (Not Found)}.
-     */
     @GetMapping("/categories/{id}")
     public ResponseEntity<CategoryDTO> getCategory(@PathVariable Long id) {
         log.debug("REST request to get Category : {}", id);
@@ -110,12 +82,6 @@ public class CategoryResource {
         return ResponseUtil.wrapOrNotFound(categoryDTO);
     }
 
-    /**
-     * {@code DELETE  /categories/:id} : delete the "id" category.
-     *
-     * @param id the id of the categoryDTO to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
     @DeleteMapping("/categories/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         log.debug("REST request to delete Category : {}", id);
